cc9ec8fe3fd1dc3529ffb62f6b003ded0ae7bdf8
Matti Luukkainen <mluukkai@iki.fi>
2019-01-17 20:07:59 +0200

    front

---
+++ build/precache-manifest.c53fbd346d24fdf09ec3e9635ab7f0c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "104ca08dab19f87270bd",
+    "url": "/static/js/main.104ca08d.chunk.js"
+  },
+  {
+    "revision": "578f4ea133a566e721fa",
+    "url": "/static/js/1.578f4ea1.chunk.js"
+  },
+  {
+    "revision": "104ca08dab19f87270bd",
+    "url": "/static/css/main.f9a47af2.chunk.css"
+  },
+  {
+    "revision": "700b814c0eee9495391ef6c5022230fe",
+    "url": "/index.html"
+  }
+];
