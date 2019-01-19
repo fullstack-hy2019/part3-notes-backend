93c5cf9d2b05bf0ab15768d80a62a78ad5656749
Matti Luukkainen <mluukkai@iki.fi>
2019-01-20 00:50:15 +0200

    env for heroku

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const bodyParser = require('body-parser') 
 const app = express()
