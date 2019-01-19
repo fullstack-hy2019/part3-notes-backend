c78e6a7ae2b76c583db88622fd05a35384f0b3e6
Matti Luukkainen <mluukkai@iki.fi>
2019-01-19 17:56:15 +0200

    part3-3

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser') 
 const app = express()
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true,
-  },
-]
+const Note = require('./models/note')
 
 const logger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -32,41 +13,19 @@ const logger = (request, response, next) => {
 }
 
 app.use(express.static('build'))
-app.use(logger)
 app.use(bodyParser.json())
+app.use(logger)
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-  return maxId + 1
-}
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes.map(note => note.toJSON()))
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
+  })
 })
 
 app.post('/api/notes', (request, response) => {
@@ -76,16 +35,23 @@ app.post('/api/notes', (request, response) => {
     return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
+})
 
-  response.json(note)
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 const error = (request, response) => {
@@ -94,7 +60,7 @@ const error = (request, response) => {
 
 app.use(error)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb://fullstack:${password}@ds161224.mlab.com:61224/fullstack2019-notes`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const Note = mongoose.model('Note', {
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const note = new Note({
+  content: 'Promise auttaa asynkronisissa operaatiossa',
+  date: new Date(),
+  important: false,
+})
+
+if (false) {
+  note.save().then(response => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+}
+
+
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
