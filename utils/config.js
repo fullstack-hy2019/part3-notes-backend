c90d6f927e92f934cf2ef3526d77b939d7f65514
Matti Luukkainen <mluukkai@iki.fi>
2019-01-20 14:59:40 +0200

    fixes

--- utils/config.js
+++ utils/config.js
@@ -5,8 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
-console.log(process.env.MONGODB_URI)
-
 module.exports = {
   MONGODB_URI,
   PORT
